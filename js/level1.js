d3df77bb1c7988ca315a884fc4f8734ac5c29c6e
Thorsten Weiskopf <thorty.w82@gmail.com>
2019-12-14 20:10:35 +0100

    design and gameplay

--- js/level1.js
+++ js/level1.js
@@ -8,11 +8,12 @@ var typed;
 
 function textinput(){
     let output="";
-    if (isInputSolution(inputTextElement.value)){
-        output = ')?!-#~+<$()&?=('
+    if (isHintWords(inputTextElement.value)){
+        output = ["Du bringst mich nie zum schweigen!", "Ich bin intelligenter als du!", "Ha Ha Ha..."]
+        runTypingArr(output);
     } else { 
         if (inputTextElement.value === ""){
-            output = 'Du kannst micht nicht besiegen!'
+            output = ')?!-#~*TILT*+$()?=('            
         }        
         else if (outputTextElement.innerText === inputTextElement.value){
             output = outputTextElement.innerText +inputTextElement.value    
@@ -20,12 +21,13 @@ function textinput(){
             output = inputTextElement.value +' '+inputTextElement.value    
         }
         inputTextElement.value ='';        
+        runTyping(output);
     }
-    runTyping(output);
+    
 }
 
-function isInputSolution(word){        
-        var a = solutionWords.indexOf(word);     
+function isHintWords(word){        
+        var a = hintWords.indexOf(word);     
         if (a !== -1){
             return true
         } else {
@@ -34,13 +36,23 @@ function isInputSolution(word){
     
 }
 
+
+const hintWords = [
+    "hilfe",
+    "help",
+    "/h",
+    "?",
+    "tipp",
+];   
+
 const solutionWords = [
     "weniger",
     "less",
     "wenige",
     "keine",
     "nix",
-    "null"
+    "null",
+    "nichts"
 ];   
 
 
@@ -51,6 +63,22 @@ function runTyping(value){
     }    
     typed = new Typed('#typed', {        
         strings: [value],
+        typeSpeed: 60,
+            // time before typing starts
+            startDelay: 1000,
+            // backspacing speed
+            backSpeed: 20,
+            // time before backspacing
+            backDelay: 500  
+    });
+}
+
+function runTypingArr(value){
+    if(typed){
+        typed.destroy();
+    }    
+    typed = new Typed('#typed', {        
+        strings: value,
         typeSpeed: 60,
             // time before typing starts
             startDelay: 1000,
